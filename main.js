5cb0ebae4e89e4eacb2ffe524bd83b2a8bce500a
krystal <892184375@qq.com>
2018-04-19 18:17:13 +0800

    画画橡皮擦功能完成

--- main.js
+++ main.js
@@ -1,69 +1,91 @@
 var yyy = document.getElementById('xxx')
-
-
 var context = yyy.getContext('2d')
 
-
-pageSize()
-window.onresize = function(){
-   pageSize()
-} 
-var painting = false
-var lastPoint = {x:undefined,y:undefined}
-
-function pageSize(){
-  var pageWidth = document.documentElement.clientWidth
-  var pageHeight = document.documentElement.clientHeight
-  yyy.width = pageWidth
-  yyy.height = pageHeight
+setCanvasSize(yyy)
+listenToMouse(yyy)
+//橡皮擦与画笔按钮状态切换
+var eraserEnabled = false
+eraser.onclick = function () {
+  eraserEnabled = true
+  items.className = 'items useEraser'
 }
-//按下鼠标
-
-yyy.onmousedown = function(a){
- painting = true
-   var x = a.clientX
-    var y = a.clientY
-    lastPoint = {x:x,y:y}
-    drawCircle(x,y,2)
+brush.onclick = function () {
+  eraserEnabled = false
+  items.className = 'items'
 }
 
 
 
-//滑动鼠标
-yyy.onmousemove = function(a){
-  if(painting){
-    var x = a.clientX
-    var y = a.clientY
-    var newPoint = {x:x,y:y}
-    drawCircle(x,y,2)        
-    drawLine (lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
-    lastPoint = newPoint
+
+
+
+
+
+
+
+
+//设置画布大小
+function setCanvasSize(canvas) {
+  pageSize()
+  window.onresize = function () {
+    pageSize()
   }
-
-  
+  function pageSize() {
+    var pageWidth = document.documentElement.clientWidth
+    var pageHeight = document.documentElement.clientHeight
+    canvas.width = pageWidth
+    canvas.height = pageHeight
+  }
 }
 
-//松开鼠标
-yyy.onmouseup = function(){
-  painting = false
-}
-function drawCircle(x,y,radius){
+//画线画圆
+function drawCircle(x, y, radius) {
   context.beginPath()
   context.fillStyle = 'black'
-  context.arc(x,y,radius,0,Math.PI * 2)
+  context.arc(x, y, radius, 0, Math.PI * 2)
   context.fill()
 }
-function drawLine(x1,y1,x2,y2){
+function drawLine(x1, y1, x2, y2) {
   context.beginPath()
   context.strokeStyle = 'black'
   context.lineWidth = 5
-  context.moveTo(x1,y1)
-  
-  context.lineTo(x2,y2)
+  context.moveTo(x1, y1)
+  context.lineTo(x2, y2)
   context.stroke()
   context.closePath()
-  
 }
 
-
-
+//监听鼠标的动作
+function listenToMouse(canvas) {
+  var using = false
+  var lastPoint = { x: undefined, y: undefined }
+  //按下鼠标
+  canvas.onmousedown = function (a) {
+    var x = a.clientX
+    var y = a.clientY
+    using = true
+    if (eraserEnabled) {
+      context.clearRect(x - 5, y - 5, 10, 10)
+    } else {
+      lastPoint = { x: x, y: y }
+    }
+  }
+  //滑动鼠标
+  canvas.onmousemove = function (a) {
+    var x = a.clientX
+    var y = a.clientY
+    if (!using) { return }   //发现没有开启使用，就直接退出去，什么操作也不做
+    if (eraserEnabled) {
+      //不加这个橡皮擦开启时，不按鼠标直接移动也会清除
+      context.clearRect(x - 5, y - 5, 10, 10)
+    } else {
+      var newPoint = { x: x, y: y }
+      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+      lastPoint = newPoint
+    }
+  }
+  //松开鼠标
+  canvas.onmouseup = function () {
+    using = false
+  }
+}
