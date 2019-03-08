0f4a167bf8159332b626c54f97e656ce6108eb65
krystal <892184375@qq.com>
2019-03-08 19:16:54 +0800

    优化

--- main.js
+++ main.js
@@ -5,42 +5,15 @@ setCanvasSize(yyy)
 listenToUser(yyy)
 //切换颜色
 var colors = document.getElementById('colors')
-black.onclick = function(){
-  context.fillStyle = 'black'
-  context.strokeStyle = 'black'
+colors.addEventListener('click',function(e){
+  let eClick = e.target
+  let color = eClick.id
+  context.fillStyle = color
+  context.strokeStyle = color
   colors.querySelector('.active').classList.remove('active')
-  black.classList.add('active') 
-}
-red.onclick = function(){
-  context.fillStyle = 'red'
-  context.strokeStyle = 'red' 
-  colors.querySelector('.active').classList.remove('active')  
-  red.classList.add('active')
-}
-green.onclick = function(){
-  context.fillStyle = 'green'
-  context.strokeStyle = 'green'
-  colors.querySelector('.active').classList.remove('active')  
-  green.classList.add('active')
-}
-blue.onclick = function(){
-  context.fillStyle = 'blue'
-  context.strokeStyle = 'blue'
-  colors.querySelector('.active').classList.remove('active')  
-  blue.classList.add('active') 
-}
-yellow.onclick = function(){
-  context.fillStyle = 'yellow'
-  context.strokeStyle = 'yellow'
-  colors.querySelector('.active').classList.remove('active')  
-  yellow.classList.add('active') 
-}
-pink.onclick = function(){
-  context.fillStyle = 'pink'
-  context.strokeStyle = 'pink'
-  colors.querySelector('.active').classList.remove('active')  
-  pink.classList.add('active') 
-}
+  eClick.classList.add('active') 
+})
+
 //橡皮擦与画笔按钮状态切换
 var eraserEnabled = false
 eraser.onclick = function () {
@@ -55,20 +28,35 @@ pen.onclick = function () {
 }
 //清除当前画布
 clear.onclick = function(){
-  context.clearRect(0,0,yyy.width,yyy.height)
+  // context.clearRect(0,0,yyy.width,yyy.height) 这种方法保存图片时背景会变透明
+  context.fillStyle = "white"
+  context.beginPath() 
+  context.fillRect(0,0,yyy.width,yyy.height)  
+  context.closePath() 
 }
 //切换粗细
 var lineSize = 5
-thin.onclick = function(){
-  lineSize = 4
-  thick.classList.remove('active')
-  thin.classList.add('active')  
+var sizes = document.querySelector('#sizes')
+var sizeLi = sizes.querySelectorAll('li')
+for(let i=0;i<sizeLi.length;i++){
+  sizeLi[i].addEventListener('click',function(){
+      switch(this.className){
+        case 'thin':
+         lineSize = 4
+         break;
+        case 'middle':
+         lineSize = 5
+         break;
+        case 'thick':
+         lineSize = 8
+         break;
+      }
+      sizes.querySelector('.active').classList.remove('active')
+      this.classList.add('active')  
+  })
 }
-thick.onclick = function(){
-  lineSize = 8  
-  thin.classList.remove('active')  
-  thick.classList.add('active') 
-}
+
+
 //下载到本地
 download.onclick = function(){
   var url = yyy.toDataURL('image/png')
@@ -76,7 +64,7 @@ download.onclick = function(){
   document.body.appendChild(a)
   a.href = url
   a.download = 'picture'
-  a.target = '_blank'
+  // a.target = '_blank'
   a.click()
 }  
 
@@ -84,9 +72,8 @@ download.onclick = function(){
 //设置画布大小
 function setCanvasSize(canvas) {
   pageSize()
-  window.onresize = function () {
-    pageSize()
-  }
+  window.onresize = pageSize
+  
   function pageSize() {
     var pageWidth = document.documentElement.clientWidth
     var pageHeight = document.documentElement.clientHeight
@@ -109,32 +96,35 @@ function drawLine(x1, y1, x2, y2) {
   context.stroke()
   context.closePath()
 }
+//橡皮擦
+function clearP(x,y,w,h){
+  context.clearRect(x - 5, y - 5, w, h)
+}
 
 //监听鼠标的动作
 function listenToUser(canvas) {
   if(document.body.ontouchstart !== undefined){
     //触屏设备
-    var using = false
+    var painting = false
     var lastPoint = { x: undefined, y: undefined }
     context.fillStyle = 'white'
     context.fillRect(0,0,canvas.width,canvas.height)
     canvas.ontouchstart = function(a){
       var x = a.touches[0].clientX
       var y = a.touches[0].clientY
-      using = true
+      painting = true
       if (eraserEnabled) {
-        context.clearRect(x - 5, y - 5, 10, 10)
+        clearP(x,y,10,10)
       } else {
-        
         lastPoint = { x: x, y: y }
       }
     }
     canvas.ontouchmove = function(a){
       var x = a.touches[0].clientX
       var y = a.touches[0].clientY
-      if (!using) { return }   
+      if (!painting)  return    
       if (eraserEnabled) {
-        context.clearRect(x - 5, y - 5, 10, 10)
+        clearP(x,y,10,10)
       } else {
         var newPoint = { x: x, y: y }
         drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
@@ -142,11 +132,11 @@ function listenToUser(canvas) {
       }
     }
     canvas.ontouchend = function(){
-      using = false
+      painting = false
     }
   }else{
     //非触屏设备
-    var using = false
+    var painting = false
     var lastPoint = { x: undefined, y: undefined }
     context.fillStyle = 'white'
     context.fillRect(0,0,canvas.width,canvas.height)
@@ -154,9 +144,9 @@ function listenToUser(canvas) {
     canvas.onmousedown = function (a) {
       var x = a.clientX
       var y = a.clientY
-      using = true
+      painting = true
       if (eraserEnabled) {
-        context.clearRect(x - 5, y - 5, 10, 10)
+        clearP(x,y,10,10)
       } else {
         lastPoint = { x: x, y: y }
       }
@@ -165,10 +155,10 @@ function listenToUser(canvas) {
     canvas.onmousemove = function (a) {
       var x = a.clientX
       var y = a.clientY
-      if (!using) { return }   //发现没有开启使用，就直接退出去，什么操作也不做
+      if (!painting)  return    //发现没有开启使用，就直接退出去，什么操作也不做
       if (eraserEnabled) {
         //不加这个橡皮擦开启时，不按鼠标直接移动也会清除
-        context.clearRect(x - 5, y - 5, 10, 10)
+        clearP(x,y,10,10)
       } else {
         var newPoint = { x: x, y: y }
         drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
@@ -177,10 +167,7 @@ function listenToUser(canvas) {
     }
     //松开鼠标
     canvas.onmouseup = function () {
-      using = false
+      painting = false
     }
-  
   }
-  
- 
 }
